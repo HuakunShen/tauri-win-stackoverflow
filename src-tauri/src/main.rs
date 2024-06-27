7519a5ca22902aa315313605219dadbd47d258f8
Huakun Shen <huakun.shen@huakunshen.com>
2024-06-27 08:02:08 -0400

    Remove updater plugin

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,7 +10,6 @@ fn greet(name: &str) -> String {
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_http::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
